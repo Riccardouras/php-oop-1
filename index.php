8440250eec7c3bea418737260a24832cf803ff88
[Riccardo Uras] <[riccardo.uras3@gmail.com]>
2023-06-20 17:50:23 +0200

    work in progress

--- index.php
+++ index.php
@@ -13,27 +13,23 @@ class Movie {
     }
 
     public function getDetails() {
-        return "Title: " . $this->title . "<br>Genre: " . $this->genre . "<br>Year: " . $this->year;
+        $genresVar = " ";
+        foreach ($this->genre as $genres) {
+            $genresVar .= $genres . ", ";
+        }
+        return "Title: " . $this->title . "<br>Genre: " . $this->$genresVar . "<br>Year: " . $this->year;
     }
 }
-// prima istanza
-$movie1 = new Movie("The Ring", "Horror", 2002);
-// seconda istanza
-$movie2 = new Movie("Guardiani della Galassia", "Fantascienza", 2014);
-// terza istanza
-$movie3 = new Movie("Scary Movie", "Commedia", 2000);
-
+// creo oggetto movie
+$movies = array(
+    new Movie("The Ring", ["Horror"], 2002),
+    new Movie("Guardiani della Galassia", ["Fantascienza", "Azione"], 2014),
+    new Movie("Scary Movie", ["Commedia", "Demenziale"], 2000)
+);
 // Stampa dei valori delle proprietà di Movie
 
-echo "<h2>Movie 1</h2>";
-echo $movie1->getDetails();
-echo "<br><br>";
-
-
-echo "<h2>Movie 2</h2>";
-echo $movie2->getDetails();
-echo "<br><br>";
-
-
-echo "<h2>Movie 3</h2>";
-echo $movie3->getDetails();
+foreach ($movies as $movie) {
+    echo "<h2>" . $movie->title . "</h2>";
+    echo $movie->getDetails();
+    echo "<br><br>";
+}
